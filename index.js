84de2ce6fd29c3b4a0e32e3c2410a04d9c0158ad
barakatimothy <timothybaraka39@gmail.com>
2023-06-16 05:48:38 -0400

    complete!

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(records) {
+  const winRecord = records.find(record => record.result === 'W');
+  return winRecord ? winRecord.year : undefined;
+}
+function superbowlWin(record) {
+  const winRecord = record.find(game => game.result === 'W');
+  return winRecord ? winRecord.year : undefined;
+}
